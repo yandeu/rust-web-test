b5bd53fd712794e9d3956aee38c9f91ecb960b1e
yandeu <20306025+yandeu@users.noreply.github.com>
2025-04-30 15:19:56 +0200

    Bind console.log

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
 use wasm_bindgen::prelude::*;
 
+// console.log()
+// https://rustwasm.github.io/wasm-bindgen/examples/console-log.html
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    log(&format!("Hello, {}!", name));
+}
+
 // Called by our JS entry point to run the example
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
